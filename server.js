77ad619661aa1936f517da0b0663e9c01c286010
oncallbooking <143792222+oncallbooking@users.noreply.github.com>
2025-10-23 15:47:36 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+{
+  "name": "data-visualizer-sellable",
+  "version": "1.0.0",
+  "description": "Turnkey data visualization dashboard (Chart.js + D3) - ready to sell and rebrand",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "author": "HTML + CSS + Javascript GPT-5 Thinking mini",
+  "license": "MIT",
+  "dependencies": {
+    "body-parser": "^1.20.2",
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "uuid": "^9.0.0"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  }
+}
